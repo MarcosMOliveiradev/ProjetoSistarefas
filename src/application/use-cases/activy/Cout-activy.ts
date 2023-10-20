71abd98a75c6e6eaf97dd39fcb6ed4e10129d888
Marcos Monteiro <marcos.moliveira@outlook.com>
2023-10-20 15:14:21 -0300

    a count

--- src/application/use-cases/activy/Cout-activy.ts
+++ src/application/use-cases/activy/Cout-activy.ts
@@ -20,10 +20,28 @@ export class Cout {
 
     const activy = await this.activyRepository.count(matricula)
 
+    const count: any = {}
     activy.forEach((objeto: any) => {
-      const respose = objeto.Tarefas.codigo
+      const codigo = objeto.Tarefas.codigo
+      const nome = objeto.usuario.nome
+      const matricula = objeto.usuario.matricula
+      const setor = objeto.Tarefas.setor
+      const matriculas = 'matricula'
+      const nomes = 'nomes'
+
+      if (count[codigo]) {
+        count[nomes] = nome
+        count[matriculas] = matricula
+        count[codigo]++
+        count[setor]++
+      } else {
+        count[nomes] = nome
+        count[matriculas] = matricula
+        count[codigo] = 1
+        count[setor] = 1
+      }
     })
 
-    return { activy }
+    return { count }
   }
 }
