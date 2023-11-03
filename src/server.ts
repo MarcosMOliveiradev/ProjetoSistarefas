521f7b9169a824da9c37f6b559983f5e0c179c4e
Marcos Monteiro <marcos.moliveira@outlook.com>
2023-11-03 12:54:58 -0300

    pegar ip local

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,12 @@ import { env } from './env'
 import { atividades } from './http/atividades'
 import { usuario } from './http/usuarios'
 import { tasck } from './http/tascks'
+import os from 'node:os'
 
 const app = fastify()
+// Pega o ip local
+const networkAdress = os.networkInterfaces()
+const ip = networkAdress.Ethernet[0].address
 
 app.register(cors, {
   origin: true,
@@ -34,5 +38,5 @@ app
     host: '0.0.0.0',
   })
   .then(() => {
-    console.log(`HTTP Server running in http://192.168.0.64:${env.PORT}`)
+    console.log(`HTTP Server running in ${ip}:${env.PORT}`)
   })
