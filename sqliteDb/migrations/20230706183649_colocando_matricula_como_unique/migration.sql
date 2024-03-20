5ce9c656d2f3337d3503b26f39445a6d6e61cfa3
Marcos Monteiro <marcos.moliveira@outlook.com>
2024-03-20 13:42:59 -0300

    excluindo db antigo

--- sqliteDb/migrations/20230706183649_colocando_matricula_como_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[matricula]` on the table `usuario` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "usuario_matricula_key" ON "usuario"("matricula");
--- sqliteDb/migrations/20230711031231_codigo_agora_e_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[codigo]` on the table `tarefas` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "tarefas_codigo_key" ON "tarefas"("codigo");
