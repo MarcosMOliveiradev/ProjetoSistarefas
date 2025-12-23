613ad62faa881ebabc3725f4e258dc1197a4cf29
Marcos M Oliveira <marcos.moliveira@outlook.com>
2025-12-23 15:45:38 -0300

    ajustes sql

--- Back/drizzle/0000_lonely_tigra.sql
+++ Back/drizzle/0000_lonely_tigra.sql
@@ -3,9 +3,9 @@ CREATE TYPE "public"."selo" AS ENUM('VERDE', 'VERMELHO', 'DOURADO');--> statemen
 CREATE TYPE "public"."status_presenca" AS ENUM('PENDENTE', 'PRESENTE', 'ATRASADO', 'FALTA');--> statement-breakpoint
 CREATE TYPE "public"."tipo_esperado" AS ENUM('EMPRESA', 'INSTITUICAO', 'FOLGA', 'LIBERACAO');--> statement-breakpoint
 CREATE TYPE "public"."turno" AS ENUM('MANHA', 'TARDE', 'INTEGRAL');--> statement-breakpoint
-CREATE TYPE "public"."status_option" AS ENUM('ANALIZANDO', 'EM ANDAMENTO', 'CONCLUIDO', 'CANCELADO');--> statement-breakpoint
-CREATE TYPE "public"."category_enum" AS ENUM('COMPRAS', 'ALMOXARIFADO', 'SECRETARIA', 'FINANCEIRO', 'DP', 'INFORMATICA', 'PONTO', 'SEMAC', 'SEMAL', 'PCM', 'PJA', 'OUTROS');--> statement-breakpoint
-CREATE TYPE "public"."roles" AS ENUM('TODOS', 'ALMOXARIFADO', 'COMPRAS', 'SECRETARIA', 'FINANCEIRO', 'DP', 'INFORMATICA', 'PONTO', 'SEMAC', 'SEMAL', 'PCM', 'PJA', 'OUTROS');--> statement-breakpoint
+--CREATE TYPE "public"."status_option" AS ENUM('ANALIZANDO', 'EM ANDAMENTO', 'CONCLUIDO', 'CANCELADO');--> statement-breakpoint
+-- CREATE TYPE "public"."category_enum" AS ENUM('COMPRAS', 'ALMOXARIFADO', 'SECRETARIA', 'FINANCEIRO', 'DP', 'INFORMATICA', 'PONTO', 'SEMAC', 'SEMAL', 'PCM', 'PJA', 'OUTROS');--> statement-breakpoint
+-- CREATE TYPE "public"."roles" AS ENUM('TODOS', 'ALMOXARIFADO', 'COMPRAS', 'SECRETARIA', 'FINANCEIRO', 'DP', 'INFORMATICA', 'PONTO', 'SEMAC', 'SEMAL', 'PCM', 'PJA', 'OUTROS');--> statement-breakpoint
 CREATE TABLE "analises_mensais" (
 	"id" text PRIMARY KEY NOT NULL,
 	"usuario_id" text NOT NULL,
@@ -18,23 +18,23 @@ CREATE TABLE "analises_mensais" (
 	"gerado_em" timestamp DEFAULT now()
 );
 --> statement-breakpoint
-CREATE TABLE "Atividade" (
-	"cod_atividade" integer PRIMARY KEY NOT NULL,
-	"setor" text NOT NULL,
-	"descricao" text NOT NULL,
-	"tempo_medio" double precision DEFAULT 60 NOT NULL,
-	"ativado" boolean DEFAULT true NOT NULL,
-	"usuario" text
-);
+-- CREATE TABLE "Atividade" (
+-- 	"cod_atividade" integer PRIMARY KEY NOT NULL,
+-- 	"setor" text NOT NULL,
+-- 	"descricao" text NOT NULL,
+-- 	"tempo_medio" double precision DEFAULT 60 NOT NULL,
+-- 	"ativado" boolean DEFAULT true NOT NULL,
+-- 	"usuario" text
+-- );
 --> statement-breakpoint
-CREATE TABLE "feedback" (
-	"id" text PRIMARY KEY NOT NULL,
-	"conteudo" text NOT NULL,
-	"status" "status_option" DEFAULT 'ANALIZANDO',
-	"nome" text,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL
-);
+-- CREATE TABLE "feedback" (
+-- 	"id" text PRIMARY KEY NOT NULL,
+-- 	"conteudo" text NOT NULL,
+-- 	"status" "status_option" DEFAULT 'ANALIZANDO',
+-- 	"nome" text,
+-- 	"created_at" timestamp DEFAULT now() NOT NULL,
+-- 	"updated_at" timestamp DEFAULT now() NOT NULL
+-- );
 --> statement-breakpoint
 CREATE TABLE "grupos" (
 	"id" text PRIMARY KEY NOT NULL,
@@ -45,21 +45,21 @@ CREATE TABLE "grupos" (
 	"data_fim" date
 );
 --> statement-breakpoint
-CREATE TABLE "media_roles" (
-	"media_id" text NOT NULL,
-	"role" "roles" NOT NULL,
-	CONSTRAINT "media_roles_media_id_role_pk" PRIMARY KEY("media_id","role")
-);
+-- CREATE TABLE "media_roles" (
+-- 	"media_id" text NOT NULL,
+-- 	"role" "roles" NOT NULL,
+-- 	CONSTRAINT "media_roles_media_id_role_pk" PRIMARY KEY("media_id","role")
+-- );
 --> statement-breakpoint
-CREATE TABLE "media" (
-	"id" text PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"description" text,
-	"customer_id" text NOT NULL,
-	"category" "category_enum" NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL
-);
+-- CREATE TABLE "media" (
+-- 	"id" text PRIMARY KEY NOT NULL,
+-- 	"name" text NOT NULL,
+-- 	"description" text,
+-- 	"customer_id" text NOT NULL,
+-- 	"category" "category_enum" NOT NULL,
+-- 	"created_at" timestamp DEFAULT now() NOT NULL,
+-- 	"updated_at" timestamp DEFAULT now() NOT NULL
+-- );
 --> statement-breakpoint
 CREATE TABLE "presenca" (
 	"id" text PRIMARY KEY NOT NULL,
@@ -72,21 +72,21 @@ CREATE TABLE "presenca" (
 	CONSTRAINT "presenca_user_id_data_unique" UNIQUE("user_id","data")
 );
 --> statement-breakpoint
-CREATE TABLE "tarefas" (
-	"id" text PRIMARY KEY NOT NULL,
-	"data" text NOT NULL,
-	"item" integer NOT NULL,
-	"cod_atividade" integer,
-	"qtd_folha" integer NOT NULL,
-	"id_documento" text DEFAULT '0' NOT NULL,
-	"h_inicio" integer NOT NULL,
-	"h_termino" integer NOT NULL,
-	"n_atendimento" integer NOT NULL,
-	"ativado" boolean DEFAULT true NOT NULL,
-	"usuario" text,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now()
-);
+-- CREATE TABLE "tarefas" (
+-- 	"id" text PRIMARY KEY NOT NULL,
+-- 	"data" text NOT NULL,
+-- 	"item" integer NOT NULL,
+-- 	"cod_atividade" integer,
+-- 	"qtd_folha" integer NOT NULL,
+-- 	"id_documento" text DEFAULT '0' NOT NULL,
+-- 	"h_inicio" integer NOT NULL,
+-- 	"h_termino" integer NOT NULL,
+-- 	"n_atendimento" integer NOT NULL,
+-- 	"ativado" boolean DEFAULT true NOT NULL,
+-- 	"usuario" text,
+-- 	"created_at" timestamp DEFAULT now() NOT NULL,
+-- 	"updated_at" timestamp DEFAULT now()
+-- );
 --> statement-breakpoint
 CREATE TABLE "user_grupos" (
 	"id" text PRIMARY KEY NOT NULL,
@@ -96,34 +96,34 @@ CREATE TABLE "user_grupos" (
 	"data_fim" date
 );
 --> statement-breakpoint
-CREATE TABLE "user_roles" (
-	"user_id" text NOT NULL,
-	"role" "roles" NOT NULL,
-	CONSTRAINT "user_roles_user_id_role_pk" PRIMARY KEY("user_id","role")
-);
+-- CREATE TABLE "user_roles" (
+-- 	"user_id" text NOT NULL,
+-- 	"role" "roles" NOT NULL,
+-- 	CONSTRAINT "user_roles_user_id_role_pk" PRIMARY KEY("user_id","role")
+-- );
 --> statement-breakpoint
-CREATE TABLE "user" (
-	"id" text PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"matricula" integer NOT NULL,
-	"password" text NOT NULL,
-	"avatar_url" text,
-	"turno" "turno",
-	"ativado" boolean DEFAULT true NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "user_matricula_unique" UNIQUE("matricula")
-);
+-- CREATE TABLE "user" (
+-- 	"id" text PRIMARY KEY NOT NULL,
+-- 	"name" text NOT NULL,
+-- 	"matricula" integer NOT NULL,
+-- 	"password" text NOT NULL,
+-- 	"avatar_url" text,
+-- 	"turno" "turno",
+-- 	"ativado" boolean DEFAULT true NOT NULL,
+-- 	"created_at" timestamp DEFAULT now() NOT NULL,
+-- 	"updated_at" timestamp DEFAULT now() NOT NULL,
+-- 	CONSTRAINT "user_matricula_unique" UNIQUE("matricula")
+-- );
 --> statement-breakpoint
 ALTER TABLE "analises_mensais" ADD CONSTRAINT "analises_mensais_usuario_id_user_id_fk" FOREIGN KEY ("usuario_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "Atividade" ADD CONSTRAINT "Atividade_usuario_user_id_fk" FOREIGN KEY ("usuario") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "media_roles" ADD CONSTRAINT "media_roles_media_id_media_id_fk" FOREIGN KEY ("media_id") REFERENCES "public"."media"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "media" ADD CONSTRAINT "media_customer_id_user_id_fk" FOREIGN KEY ("customer_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+-- ALTER TABLE "Atividade" ADD CONSTRAINT "Atividade_usuario_user_id_fk" FOREIGN KEY ("usuario") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+-- ALTER TABLE "media_roles" ADD CONSTRAINT "media_roles_media_id_media_id_fk" FOREIGN KEY ("media_id") REFERENCES "public"."media"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+-- ALTER TABLE "media" ADD CONSTRAINT "media_customer_id_user_id_fk" FOREIGN KEY ("customer_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "presenca" ADD CONSTRAINT "presenca_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "tarefas" ADD CONSTRAINT "tarefas_cod_atividade_Atividade_cod_atividade_fk" FOREIGN KEY ("cod_atividade") REFERENCES "public"."Atividade"("cod_atividade") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "tarefas" ADD CONSTRAINT "tarefas_usuario_user_id_fk" FOREIGN KEY ("usuario") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+-- ALTER TABLE "tarefas" ADD CONSTRAINT "tarefas_cod_atividade_Atividade_cod_atividade_fk" FOREIGN KEY ("cod_atividade") REFERENCES "public"."Atividade"("cod_atividade") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+-- ALTER TABLE "tarefas" ADD CONSTRAINT "tarefas_usuario_user_id_fk" FOREIGN KEY ("usuario") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "user_grupos" ADD CONSTRAINT "user_grupos_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "user_grupos" ADD CONSTRAINT "user_grupos_grupo_id_grupos_id_fk" FOREIGN KEY ("grupo_id") REFERENCES "public"."grupos"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "user_roles" ADD CONSTRAINT "user_roles_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-CREATE INDEX "media_roles_role_idx" ON "media_roles" USING btree ("role");--> statement-breakpoint
-CREATE INDEX "user_roles_role_idx" ON "user_roles" USING btree ("role");
+-- ALTER TABLE "user_roles" ADD CONSTRAINT "user_roles_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+-- CREATE INDEX "media_roles_role_idx" ON "media_roles" USING btree ("role");--> statement-breakpoint
+-- CREATE INDEX "user_roles_role_idx" ON "user_roles" USING btree ("role");
