36920d38db76e04e16366bc251e38c0d4b7582a9
Marcos Monteiro <marcos.moliveira@outlook.com>
2026-01-12 14:19:57 -0300

    Config node-cron

--- Back/src/application/useCase/grupos/function/criaPresencaJob.ts
+++ Back/src/application/useCase/grupos/function/criaPresencaJob.ts
@@ -1,12 +1,14 @@
 import cron from 'node-cron'
 import { makeGeraPresencaDiaJob } from '../factories/make-gera-presenca-dia-job.ts'
 
-cron.schedule("30 6 * * *", async() => {
+cron.schedule("30 06 * * *", async() => {
   const hoje = new Date()
   hoje.setUTCHours(0, 0, 0, 0)
 
   const job = makeGeraPresencaDiaJob()
   await job.execute(hoje)
+
+  console.log("Job executado com sucesso")
 }, {
   timezone: "America/Sao_Paulo",
 })
--- Back/src/application/useCase/grupos/function/fechaPresencaJob.ts
+++ Back/src/application/useCase/grupos/function/fechaPresencaJob.ts
@@ -1,13 +1,15 @@
 import cron from "node-cron";
 import { makeFecharPresencasPendentes } from "../factories/make-fechar-presencas-pendentes.ts";
 
-cron.schedule("20 13 * * *", async () => {
+cron.schedule("20 17 * * *", async () => {
   const job = makeFecharPresencasPendentes();
 
   const hoje = new Date();
   hoje.setUTCHours(0, 0, 0, 0);
 
   await job.execute(hoje);
+
+  console.log("Job executado com sucesso")
 }, {
   timezone: "America/Sao_Paulo",
 });
