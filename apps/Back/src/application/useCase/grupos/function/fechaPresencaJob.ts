70add20c706ed2641969db7a60a5e7dd93a19a9c
Marcos Monteiro <marcos.moliveira@outlook.com>
2026-08-06 10:59:48 -0300

    fix: transformando sistema em mono repo com turbo repo

---
+++ apps/Back/src/application/useCase/grupos/function/fechaPresencaJob.ts
@@ -0,0 +1,17 @@
+import cron from "node-cron";
+import { makeFecharPresencasPendentes } from "../factories/make-fechar-presencas-pendentes.ts";
+
+export function registerFechaPresencaJob() {
+  cron.schedule("20 17 * * *", async () => {
+    const job = makeFecharPresencasPendentes();
+  
+    const hoje = new Date();
+    hoje.setUTCHours(0, 0, 0, 0);
+  
+    await job.execute(hoje);
+  
+    console.log("Job executado com sucesso")
+  }, {
+    timezone: "America/Sao_Paulo",
+  });
+}
--- apps/Back/src/server.ts
+++ apps/Back/src/server.ts
@@ -1,6 +1,8 @@
 import { app } from "./app.ts";
+import { registerJobs } from "./application/useCase/grupos/function/index.ts";
 import { env } from "./lib/env.ts";
 
+registerJobs()
 
 app.listen({
     host:'0.0.0.0',
